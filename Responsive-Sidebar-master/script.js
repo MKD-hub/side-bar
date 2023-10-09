fa20b6a0a3b4a0e157ce2e5cdbbc075723ebfc6b
MKD-hub <mkdiab58@gmail.com>
2023-10-09 18:17:48 +0300

    Dropdowns and transitions

---
+++ Responsive-Sidebar-master/script.js
@@ -0,0 +1,43 @@
+const alfursanDropDown = document.getElementById('Alfursan');
+const alfursanContent = document.querySelector('.alfursan-content')
+
+const dropdownButton = document.getElementById('Aircrafts');
+const dropdownContent = document.querySelector('.dropdown-content');
+
+const planeNavList = document.querySelector('.plane-nav-list');
+
+
+const navlist = document.querySelector('.nav_list');
+const alfursanNavList = document.querySelector('.alfursan-nav-list');
+
+
+const backbtn = document.getElementById('btn-back');
+
+backbtn.addEventListener('click', function (){
+    navlist.style.display = "flex";
+    alfursanNavList.classList.remove('show');
+})
+
+dropdownButton.addEventListener('click', function () {
+    dropdownContent.classList.toggle('show-dropdown');
+});
+  
+alfursanDropDown.addEventListener('click', function () {
+      alfursanContent.classList.toggle('show-dropdown');
+});
+
+function handlePlaneTransition() {
+    navlist.style.display = 'none';
+    planeNavList.classList.toggle('show');
+}
+
+function handleAlfursanTransition() {
+    navlist.style.display = 'none';
+    alfursanNavList.classList.toggle('show');
+}
+
+
+
+
+
+
